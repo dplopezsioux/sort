d4ef98c2825c452fa055992d5481b5c59ce3be20
Daniel <dplopez.sioux@gmail.com>
2021-09-01 12:42:16 -0500

    Update sort.js
    update

--- sort.js
+++ sort.js
@@ -1,13 +1,14 @@
 module.exports = function sortCategoriesForInsert(inputJson) {
   let properJsonOutput = [];
-  
+
   if (typeof inputJson == "string") properJsonOutput = JSON.parse(inputJson);
   else properJsonOutput = [].concat(inputJson);
 
   properJsonOutput.sort((a, b) => {
     if (a.parent_id == null) return -1;
-    else return 1;
+
+    return a.parent_id - b.id;
   });
 
   return properJsonOutput;
-}
+};
