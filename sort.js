e1e590a1927652bac23a52b1d25da53958444963
Daniel <38842757+dplopezsioux@users.noreply.github.com>
2021-09-01 21:38:22 -0500

    end
    end

--- sort.js
+++ sort.js
@@ -1,16 +1,28 @@
-module.exports = function sortCategoriesForInsert(inputJson) {
+module.exports =
+function sortCategoriesForInsert(inputJson) {
   let properJsonOutput = [];
 
   if (typeof inputJson == "string") properJsonOutput = JSON.parse(inputJson);
   else properJsonOutput = [].concat(inputJson);
 
-properJsonOutput.sort((a, b) => {
-    if (a.parent_id == null) return -1;
-
-    let pID = a.parent_id;
-    if (a.parent_id == null) pID = -1;
-    return pID - b.id;
-  });
-  
+  organiz();
+  function organiz() {
+    for (let indexP = 0; indexP < properJsonOutput.length; indexP++) {
+      const element = properJsonOutput[indexP];
+      if (element.parent_id == null)
+      {
+        properJsonOutput.splice(indexP,1);
+        properJsonOutput.unshift(element);
+      }
+      for (let ind = 0; ind < properJsonOutput.length; ind++) {
+        const ele = properJsonOutput[ind];
+        if (ele.id == element.parent_id && indexP < ind) {
+          properJsonOutput.splice(ind+1,0,element);
+          properJsonOutput.splice(indexP,1);
+          organiz();
+        }
+      }
+    }
+  }
   return properJsonOutput;
-};
+}
